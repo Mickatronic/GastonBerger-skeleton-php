5d23243494fc669a03b6cedaf5ab06a43e46a3fd
HONVAULT <honvault.m@gmail.com>
2019-01-28 18:02:18 +0100

    Ajout du framework bootstrap

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -1,5 +1,12 @@
 <div id="footer">
     Footer content...
 </div>
+
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+
+<script src="js/bootstrap.bundle.min.js" ></script>
+<script src="js/bootstrap.min.js" ></script>
+
+
 </body>
 </html>
